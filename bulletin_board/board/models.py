c9cf8c5551b0265b0d6cd02010d4941e593ede4c
shrhkddh <shrhkddh0244@gmail.com>
2020-08-12 16:34:04 +0900

    [Add]board app 작성

--- bulletin_board/board/models.py
+++ bulletin_board/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    name        = models.CharField(max_length = 100)
+    password    = models.CharField(max_length = 500)
+    title       = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 5000)
+    updated_at  = models.DateField(auto_now = True)
+
+    class Meta:
+        db_table = 'boards'
